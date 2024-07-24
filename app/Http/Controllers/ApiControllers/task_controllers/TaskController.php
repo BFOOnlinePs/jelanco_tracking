34dfb38f06fc9aed887e092174239c8ee2f3b19f
Aseel-Mohtaseb <aseelamermohtaseb@gmail.com>
2024-07-24 09:47:23 +0300

    submissions media separated

--- app/Http/Controllers/ApiControllers/task_controllers/TaskController.php
+++ app/Http/Controllers/ApiControllers/task_controllers/TaskController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers\ApiControllers\task_controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\AttachmentsModel;
 use App\Models\TaskModel;
 use App\Models\TaskSubmissionCommentsModel;
 use App\Models\TaskSubmissionsModel;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use SplFileInfo;
 
 class TaskController extends Controller
 {
+
 
     public function getAllTasks()
     {
@@ -22,15 +26,20 @@ class TaskController extends Controller
 
     public function getTaskWithSubmissionsAndComments($id)
     {
+
         $task = TaskModel::where('t_id', $id)
             ->with('taskCategory:c_id,c_name')
             ->with('addedByUser:id,name')
             ->first();
 
         if ($task) {
+            $imageTypes = config('filetypes.image_types');
+            $videoTypes = config('filetypes.video_types');
+
             $task->task_submissions = TaskSubmissionsModel::where('ts_task_id', $id)->get();
             $task->assigned_to_users = User::whereIn('id', json_decode($task->t_assigned_to))->select('id', 'name')->get();
-            $task->task_submissions->transform(function ($submission) {
+
+            $task->task_submissions->transform(function ($submission) use ($imageTypes, $videoTypes) {
                 // to get the submitter user
                 $user_id = $submission->ts_submitter;
                 $user = User::where('id', $user_id)->select('id', 'name')->first();
@@ -44,6 +53,38 @@ class TaskController extends Controller
                 });
 
                 // to get task media (images + videos + files)
+
+                $images = [];
+                $videos = [];
+                $files = [];
+
+                // $submission->submission_attachments = AttachmentsModel::where('a_table', 'task_submissions')
+                //     ->where('a_fk_id', $submission->ts_id)
+                //     ->get();
+
+                $attachments = AttachmentsModel::where('a_table', 'task_submissions')
+                    ->where('a_fk_id', $submission->ts_id)
+                    ->get();
+
+                foreach ($attachments as $attachment) {
+                    $extension = strtolower(pathinfo($attachment->a_attachment, PATHINFO_EXTENSION));
+
+                    // Log::info('File extension: ' . $extension);
+
+                    if (in_array($extension, $imageTypes)) {
+                        $images[] = $attachment;
+                    } elseif (in_array($extension, $videoTypes)) {
+                        $videos[] = $attachment;
+                    } else {
+                        $files[] = $attachment;
+                    }
+                }
+
+                $submission->submission_attachments = [
+                    'images' => $images,
+                    'videos' => $videos,
+                    'files' => $files,
+                ];
 
 
                 return $submission;
@@ -85,10 +126,10 @@ class TaskController extends Controller
         }
 
         $auth_id = auth()->user()->id;
-// return  $request->input('start_time');
+        // return  $request->input('start_time');
         $task = new TaskModel();
         $task->t_content = $request->input('content');
-        $task->t_planed_start_time = $request->input('start_time') ;
+        $task->t_planed_start_time = $request->input('start_time');
         $task->t_planed_end_time = $request->input('end_time');
         $task->t_status = 'active'; // default
         $task->t_category_id = $request->input('category_id');
